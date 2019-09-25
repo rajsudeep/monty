a21f40b464e1c53daf748817890b9f56e19dda62
rajsudeep <sudrocks@gmail.com>
2019-09-25 19:07:56 +0000

    Set return value to func pointer

--- get_op.c
+++ get_op.c
@@ -5,38 +5,17 @@
  *
  *
  */
-int (*get_op(char *opcode)) (stack_t **stack, unsigned int line_number)
+void (*get_op(char *opcode))(stack_t **stack, unsigned int line_number)
 {
-	op_t ops[] = {
-		{"push", push},
-		{"pall", pall},
-		{"pint", pint},
-		{"pop", pop},
-		{"swap", swap},
-		{"add", add},
-		{"nop", nop},
+	instruction_t ops[] = {
+		/*{"push", push_node},
+		  {"pall", pall_node},*/
 		{NULL, NULL}
 	};
 	int i;
 
-	for (i = 0; ops.opcode[i] != NULL ; i++)
+	for (i = 0; ops[i].opcode != NULL ; i++)
 		if (_cmpstrandlen(ops[i].opcode, opcode) == 0)
-			return (ops[i].f);
+			return (*(ops[i]).f);
 	return (NULL);
-}
-
-
-/**
- * _cmpstrandlen - compares two strings and compares the length of the strings
- * @s1: first string to compare
- * @s2: second string to compare
- *
- * Return: If strings are the "same" return 0 else 1
- */
-int _cmpstrandlen(char *s1, char *s2)
-{
-	if (!_strcmp(s1, s2) && (_strlen(s1) == _strlen(s2)))
-		return (0);
-	else
-		return (1);
 }
