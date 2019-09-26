c19b2866570fdc9f9c310a7ded78cc1722c2c24e
rajsudeep <sudrocks@gmail.com>
2019-09-26 18:56:38 +0000

    Fix typos on div op

--- list_helpers3.c
+++ list_helpers3.c
@@ -1,17 +1,22 @@
 #include "monty.h"
 
+/**
+ * div_node - divide the first two nodes
+ * @stack: pointer to stack
+ * @line_number: count of number of commands
+ */
 void div_node(stack_t **stack, unsigned int line_number)
 {
-        int n_div = 0;
+	int n_div = 0;
 
-        if (*stack == NULL || (*stack)->next == NULL)
-        {
-                fprintf(stderr, "L%u: can't div, stack too short\n",
-                        line_number);
-                free(data.line), free_stack(data.head), fclose(data.fp);
-                exit(EXIT_FAILURE);
-        }
-        n_div = (*stack)->n;
-        pop_node(stack, line_number);
-        (*stack)->n /= n_div;
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't div, stack too short\n",
+			line_number);
+		free(data.line), free_stack(data.head), fclose(data.fp);
+		exit(EXIT_FAILURE);
+	}
+	n_div = (*stack)->n;
+	pop_node(stack, line_number);
+	(*stack)->n /= n_div;
 }
