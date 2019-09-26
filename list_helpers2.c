542266a38dd565ada5b045be39083baa8b13b511
rajsudeep <sudrocks@gmail.com>
2019-09-26 06:30:32 +0000

    Test swap op using tmp nodes

--- list_helpers2.c
+++ list_helpers2.c
@@ -7,6 +7,8 @@
  */
 void swap_node(stack_t **stack, unsigned int line_number)
 {
+	stack_t *tmp = *stack;
+	stack_t *tmp2 = tmp->next;
 	int head_n;
 
 	if (*stack == NULL || (*stack)->next == NULL)
@@ -16,9 +18,9 @@ void swap_node(stack_t **stack, unsigned int line_number)
 		free(data.line), free_stack(data.head), fclose(data.fp);
 		exit(EXIT_FAILURE);
 	}
-	head_n = (*stack)->n;
-	(*stack)->n = (*stack)->next->n;
-	(*stack)->next->n = head_n;
+	head_n = tmp->n;
+	tmp->n = tmp2->n;
+	tmp2->n = head_n;
 }
 
 /**
