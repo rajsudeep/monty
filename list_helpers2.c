aeed9f9727d9737c928dd74824a789d389aed439
rajsudeep <sudrocks@gmail.com>
2019-09-26 06:40:03 +0000

    Implement add op func

--- list_helpers2.c
+++ list_helpers2.c
@@ -22,6 +22,28 @@ void swap_node(stack_t **stack, unsigned int line_number)
 }
 
 /**
+ * add_node - add together the first two nodes
+ * @stack: pointer to stack
+ * @line_number: count of number of commands
+ */
+void add_node(stack_t **stack, unsigned int line_number)
+{
+	int n_sum = 0;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+        {
+                fprintf(stderr, "L%u: can't add, stack too short\n",
+                        line_number);
+                free(data.line), free_stack(data.head), fclose(data.fp);
+                exit(EXIT_FAILURE);
+        }
+	n_sum = (*stack)->n;
+	pop_node(stack, line_number);
+	n_sum += (*stack)->n;
+	(*stack)->n = n_sum;
+}
+
+/**
  * nop_nop - does nothing
  * @stack: pointer to stack
  * @line_number: count of number of commands
